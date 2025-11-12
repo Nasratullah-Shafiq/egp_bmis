6c7887fde79aca6864eaac63fbc9389afa634473
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2025-11-12 09:31:25 +0430

    new changes

--- models/construction_pro.py
+++ models/construction_pro.py
@@ -1,7 +1,7 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError, ValidationError
 from collections import defaultdict
-
+ 
 
 class ConstructionControl(models.Model):
     """
